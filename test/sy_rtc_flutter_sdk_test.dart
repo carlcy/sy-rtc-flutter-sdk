4525dc86a3f31168bc85ce92364469a95288b3d5
carlcy <carlcy2023@163.com>
2026-01-21 09:11:06 +0800

    test: mock method channel for engine init/join

--- test/sy_rtc_flutter_sdk_test.dart
+++ test/sy_rtc_flutter_sdk_test.dart
@@ -1,7 +1,36 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart';
 import 'package:sy_rtc_flutter_sdk/sy_rtc_flutter_sdk.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  const MethodChannel channel = MethodChannel('sy_rtc_flutter_sdk');
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      channel,
+      (MethodCall methodCall) async {
+        // 基础 mock：只要不抛 MissingPluginException 即可
+        switch (methodCall.method) {
+          case 'init':
+          case 'checkFeatures':
+          case 'join':
+          case 'leave':
+            return true;
+          case 'hasFeature':
+            return true;
+          default:
+            return null;
+        }
+      },
+    );
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+  });
+
   test('SyRtcEngine 单例测试', () {
     final engine1 = SyRtcEngine();
     final engine2 = SyRtcEngine();
